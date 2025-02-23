ea9e3532fc41e726852384c28914193b736eabd1
Naresh Bhatia <me@nareshbhatia.dev>
2025-02-23 14:01:56 -0500

    initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'Form Experiments',
+  description: 'Form Experiments',
+  url: 'https://form-experiments.vercel.app',
+  ogImage: {
+    url: 'https://form-experiments.vercel.app/og.png',
+    alt: 'Form Experiments',
+    width: 1200,
+    height: 630,
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
